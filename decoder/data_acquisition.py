6eeb46155c2a9bbaf7df5cb2952459fb25b78ece
rtcheng <38085192+rtcheng1@users.noreply.github.com>
2020-11-09 11:33:38 -0800

    Randall Cheng
    Update to script

--- decoder/data_acquisition.py
+++ decoder/data_acquisition.py
@@ -1,11 +1,21 @@
+import time
+import json
 import requests
+from requests.exceptions import HTTPError
+
+ t = 0
 
 try:
 	print('Requesting data from localhost')
 	url = 'http://127.0.0.1:8080/data.json'
 	feedback = requests.get(url)
 	acquired_data = feedback.json()
-	print(acquired_data)
+	while True:
+		t = t + 1
+		print(acquired_data)
+		#print(feedback.status_code)
+		print(t + 'seconds passed')
+		time.sleep(10)
 
 except Exception as e:
     print(f'Error while parsing URL occurred: {e}')
