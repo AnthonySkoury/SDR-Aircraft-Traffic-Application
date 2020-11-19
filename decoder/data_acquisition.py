2a6226927d61f4e55f67671d1911ebe046ee4803
rtcheng <38085192+rtcheng1@users.noreply.github.com>
2020-11-18 19:42:50 -0800

    Randall Cheng
    Update to script for sending ADS-B data to backend server

--- decoder/data_acquisition.py
+++ decoder/data_acquisition.py
@@ -1,27 +1,49 @@
 import time
+import datetime
 import json
 import requests
 from requests.exceptions import HTTPError
 
 t = 0
 tot = 0
-incr = 10
+incr = 1
 s_p = " seconds passed"
+t_s = 0
+
+url = 'http://127.0.0.1:8080/data.json'
+url_2 = 'http://localhost:8080/api/aircraft/'
+url_3 = 'http://localhost:8080/api/datarecord/'
 
 try:
     print('Requesting data from localhost')
-    url = 'http://127.0.0.1:8080/data.json'
     feedback = requests.get(url)
     acquired_data = feedback.json()
     while True:
         tot += incr
-        print(acquired_data)
-        send_back = json.dumps(acquired_data, indent = 13)
+        data_1 = []
+        data_2 = []
+        for i in acquired_data:
+            data1 = {}
+            data2 = {}
+            data1['icao'] = i['hex']
+            data2['icao'] = i['hex']
+            #data1['squawk'] = i['squawk']
+            data1['callsign'] = i['flight']
+            data2['latitude'] = i['lat']
+            data2['longitude'] = i['lon']
+            #data2['validposition'] = i['validposition']
+            data2['altitude'] = i['altitude']
+            #data2['vert_rate'] = i['vert_rate']
+            #data2['track'] = i['track']
+            #data2['validtrack'] = i['validtrack']
+            data2['ground_speed'] = i['speed']
+            #data2['messages'] = i['messages']
+            #data2['seen'] = i['seen']
+            data_1.append(data1)
+            data_2.append(data2)
+            send_back_1 = requests.post(url_2,data=data1)
+            send_back_2 = requests.post(url_3,data=data2)
 
-        with open("acquired_data.json","w") as outfile:
-            outfile.write(send_back)
-
-        print(str(tot) + s_p)
         time.sleep(incr)
 
 except HTTPError as http_err:
